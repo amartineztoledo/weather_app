28e0216c2cda53e733af77f11704e4d57cf75d8b
Alvaro Martinez <alvaromartineztoledo@gmail.com>
2018-11-02 15:55:47 -0300

    Deleted unused welcome_controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
       get :update_temps
     end
   end
-  get 'welcome/index'
   root :to => "cities#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
